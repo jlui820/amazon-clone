2ebc5717f6ea163b4cd0436eb2d2af277fb681aa
Jeff <jlui820@gmail.com>
2020-10-12 20:04:40 -0700

    start amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyADWxbsC4cM9fyJpVLcsvG2AbOcjEYFiC4',
+  authDomain: 'clone-34e55.firebaseapp.com',
+  databaseURL: 'https://clone-34e55.firebaseio.com',
+  projectId: 'clone-34e55',
+  storageBucket: 'clone-34e55.appspot.com',
+  messagingSenderId: '190962953855',
+  appId: '1:190962953855:web:317624c81effa3bae4d94a',
+  measurementId: 'G-EQRXYL142F',
+};
